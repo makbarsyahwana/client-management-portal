6896e1c992d418a588c8056985f170b3ff7dd431
makbarsyahwana <makbarsyahwana@gmail.com>
2022-10-05 17:54:22 +0700

    client card json

---
+++ utills/cards-data.ts
@@ -0,0 +1,19 @@
+const cardData = [
+    {
+        logo: "/dbs-logo.png",
+        title: "DBS Bank",
+        text: "DBS Bank Limited is a Singaporean multinational banking and financial services corporation headquartered at the Marina Bay Financial Centre in the Marina Bay district of Singapore."
+    },
+    {
+        logo: "/proudfoot-logo.png",
+        title: "Proudfoot",
+        text: "Proudfoot engages teams and leadership, at all levels, to create innovative solutions to operational constraints and solve the people challenge associated with making sure that change takes place."
+    },
+    {
+        logo: "/rmi-logo.png",
+        title: "RMI",
+        text: "RMI is a trusted global verification partner for Asia-Pacific organisations, offering a highly personalised and comprehensive background screening service."
+    }
+]
+
+export default cardData
