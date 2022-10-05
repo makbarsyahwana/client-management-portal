3971bd4cf1a91a9450083427a2b9dec2bc4deb15
makbarsyahwana <makbarsyahwana@gmail.com>
2022-10-05 17:52:20 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      nunito: ['Nunito', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+  mode: 'jit',
+}
